43970fa56c4bfe555db58a9d942551a87c6c15e7
mariobet23440 <bet23440@uvg.edu.gt>
2025-02-13 10:25:15 -0600

    Código Trabajado durante el laboratorio

--- Sumador_4_Bits/Sumador_4_Bits/main.asm
+++ Sumador_4_Bits/Sumador_4_Bits/main.asm
@@ -8,15 +8,15 @@
 ; pero creé este archivo para mostrar que si lo hice yo xd.
 
 
-    .include "M328PDEF.inc"
-    .cseg
-    .org    0x0000
+.include "M328PDEF.inc"
+.cseg
+.org    0x0000
 
-    // Configurar la pila
-    LDI     R16, LOW(RAMEND)
-    OUT     SPL, R16
-    LDI     R16, HIGH(RAMEND)
-    OUT     SPH, R16
+// Configurar la pila
+LDI     R16, LOW(RAMEND)
+OUT     SPL, R16
+LDI     R16, HIGH(RAMEND)
+OUT     SPH, R16
 
 SETUP:
     // Activación de pines de salida en el puerto B
@@ -25,29 +25,103 @@ SETUP:
     LDI     R16, 0x00
     OUT     PORTB, R16
 
+	// Activación de pines de entrada en el puerto C
+    LDI     R16, 0x00
+    OUT     DDRB, R16
+    LDI     R16, 0xFF		// Activar Pull-ups
+    OUT     PORTB, R16
+
     // Activación de pines de salida en el puerto D
     LDI     R16, 0xFF
     OUT     DDRD, R16
     LDI     R16, 0x00
     OUT     PORTD, R16
 
-    // Registros de contadores
-    LDI     R21, 0x00
-	LDI     R22, 0x00
 
 MAINLOOP:
-    // Incrementar contadores y sacarlos en los puertos
-    INC     R21
-    OUT     PORTB, R21
-    CALL    DELAY_255_POW3
+	// Ejecutar incremento y decremento de contadores
+	CALL	CONTADOR1
+	CALL	CONTADOR2
 
-	INC     R22
-    OUT     PORTD, R22
-    CALL    DELAY_255_POW3
+	// Una vez que se haya regresado, debemos unir los bits de R16 y R20
+	// Para colocar los bits de R16 como los últimos bits de PORTD y los bits de R20 como los primeros
+	// debemos desplazar los bits de R20 4 bits a la izquierda
 
-    RJMP    MAINLOOP
+	// Afortunadamente ya existe una función precisamente para eso
+	MOV		R21, R20
+	SWAP	R21 // Con esto intercambiamos los nibbles de R20
+
+	// Ahora unimos las dos cosas
+	OR		R16, R21
+
+	// Finalmente las sacamos en PORTD
+	OUT		PORTD, R16
 
 
+	// Repetir todo
+	RJMP MAINLOOP
+
+CONTADOR1:
+	// Guardamos el valor de PORTB en R17 (Aquí van las dos entradas)
+	IN		R17, PORTB
+
+	// Cambio de flujo - Decremento
+	SBRC	R17, 0
+	CALL	DELAY_SETUP
+	SBRC	R17, 0 
+	DEC		R16
+
+	// Cambio de flujo - Incremento
+	SBRC	R17, 1
+	CALL	DELAY_SETUP
+	SBRC	R17, 1
+	INC		R16
+
+	// Truncar resultado a sólo cuatro bits
+	ANDI	R16, 0x0F
+
+	// Mostrar el resultado en PORTD
+	// OUT		PORTD, R16
+
+	// Regresar a MAINLOOP
+	RET
+
+CONTADOR2:
+	// Para este contador usaremos los bits 2 y 3 de PORTB
+	// El bit 2 servirá para decrementar el valor del contador
+	// El bit 3 se usará para incrementar el valor del contador
+
+	// Guardamos el valor de PORTB en R17 (Aquí van las dos entradas)
+	IN		R17, PORTB
+
+	// Cambio de flujo - Decremento
+	SBRC	R17, 2
+	CALL	DELAY_SETUP
+	SBRC	R17, 2 
+	DEC		R20
+
+	// Cambio de flujo - Incremento
+	SBRC	R17, 3
+	CALL	DELAY_SETUP
+	SBRC	R17, 3
+	INC		R20
+
+	// Truncar resultado a sólo cuatro bits
+	ANDI	R20, 0x0F
+
+	// Desplazar los 4 bits hacia la izquierda
+
+
+	// Mostrar el resultado en PORTD
+	// OUT		PORTD, R20
+	// Podemos ver que el registro R20
+
+	// Regresar a MAINLOOP
+	RET
+
+
+
+// NO TOCAR ------------------------------------------------------------------------
 // Delay con conteo
 DELAY_255_POW3:
     LDI     R18, 32		// ESTABLECER R18 AQUÍ
