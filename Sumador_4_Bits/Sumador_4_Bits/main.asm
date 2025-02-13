fce3b43ea9c02b92fcb25cc7d3b6be896c122ad6
mariobet23440 <bet23440@uvg.edu.gt>
2025-02-13 10:18:39 -0600

    Nuevos comentarios iniciales

---
+++ Sumador_4_Bits/Sumador_4_Bits/main.asm
@@ -0,0 +1,15 @@
+;
+; Sumador_4_Bits.asm
+;
+; Created: 13/02/2025 10:15:38
+; Author : Mario Alejandro Betancourt Franco
+; Nota importante: El uC no me funcionaba durante el lab. Encontramos con Hector que era un problema de Drivers
+; Nota importante 2: Olvidé crear el repositorio al inicio del lab. Voy a subir ese archivo como muestra de que si trabajé ese día
+; pero creé este archivo para mostrar que si lo hice yo xd.
+
+
+; Replace with your application code
+start:
+    inc r16
+    rjmp start
+
